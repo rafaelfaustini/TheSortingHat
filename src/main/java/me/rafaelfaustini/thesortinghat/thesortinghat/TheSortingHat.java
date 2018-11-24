ce40b0045d18d95220fe7ef571e0176c8eff3748
Rafael Faustini Costa de Oliveira <contato@rafaelfaustini.com.br>
2018-11-24 17:48:54 -0200

    Initial commit

---
+++ src/main/java/me/rafaelfaustini/thesortinghat/thesortinghat/TheSortingHat.java
@@ -0,0 +1,22 @@
+package me.rafaelfaustini.thesortinghat;
+
+import me.rafaelfaustini.thesortinghat.commands.Sort;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class TheSortingHat extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        System.out.println("[TheSortingHat] Plugin activated successfully");
+        getConfig().options().copyDefaults();
+        saveDefaultConfig();
+        getCommand("sort").setExecutor(new Sort());
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
